6cd1ac84e006590f6d81c4eeb1104236043e6c41
Zhang San <328367803+goodnewboy@users.noreply.github.com>
2026-09-13 11:17:39 +0800

    初始化记账本项目12345

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def add_expense():
     name = input("花费名称：")
     amount = float(input("金额："))
-    print(f"已记录：{name}，{amount}元32")
+    print(f"已记录：{name}，{amount}元12345")
 
 if __name__ == "__main__":
     add_expense()
